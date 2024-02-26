9256126badef64f18995f719f433dae2fa24df4b
Luca Adam <luca.adam23@gmail.com>
2024-02-26 07:06:57 +0300

    Ajout prix fornisseur

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -65,7 +65,7 @@ const routes: Routes = [
         canActivate: [ApplicationGuardService]
       },
       {
-        path: 'statistiques',
+        path: 'graph',
         component: PageStatistiquesComponent,
         canActivate: [ApplicationGuardService]
       },
